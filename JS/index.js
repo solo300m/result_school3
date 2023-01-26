6f2a6345ff8f56620a34df0f8625c3a46fbbbcf1
Sergey Lyulchenko <solo300m@yandex.ru>
2023-01-26 10:30:32 +0400

    commit init

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+/*function timer(numb){
+    let time = document.querySelector('.timer_value')
+    time.textContent = numb
+}
+
+function start(count){
+    for(var i = 0; i < count; i++){
+        setTimeout(timer(i),1000)        
+    }
+}*/
+//start(3);
+console.log("Старт обратного отсчета!")
+
+console.log(5)
+alert(5)
+
+console.log(4)
+
+alert(4)
+console.log(3)
+
+alert(3)
+console.log(2)
+
+alert(2)
+console.log(1)
+
+alert(1)
+console.log("Отсчет закончен!")
+
+alert(0)
+
